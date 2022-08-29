3b1c0a6affaadbea25d5353d38dd54cc7cc3b86d
rjarenas <ruben.arenas.28@gmail.com>
2022-08-29 15:12:06 +0700

    add production db connection

--- src/database-connection.js
+++ src/database-connection.js
@@ -1,9 +1,20 @@
-const DATABASE_CONNECTION = {
-    user: 'todo_user',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'root',
-    port: 5432,
-};
+if (process.env.NODE_ENV == 'production') {
+    const DATABASE_CONNECTION = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    };
+}
+else {
+    const DATABASE_CONNECTION = {
+        user: 'todo_user',
+        host: 'localhost',
+        database: 'postgres',
+        password: 'root',
+        port: 5432,
+    };
+    
+}
 
 exports.DATABASE_CONNECTION = DATABASE_CONNECTION;
